9e49db7c9e39a22fa2892971d8fd10a22c238c91
Lucas Vazquez <vazquezlucasmatias2001@gmail.com>
2023-02-27 19:14:15 -0300

    Ejemplo Finally
    Finalizada

--- Program.cs
+++ Program.cs
@@ -7,7 +7,43 @@ namespace EjemploFinally
         static void Main(string[] args)
         {
 
+            System.IO.StreamReader archivo = null;
 
+            try
+            {
+
+                string linea;
+
+                int contador = 0;
+
+                string path = @"C:\Users\vazqu\OneDrive\Escritorio\Ruta.txt";
+
+                archivo=new System.IO.StreamReader(path);
+
+                while((linea=archivo.ReadLine()) != null)
+                {
+
+                    Console.WriteLine(linea);
+
+                    contador++;
+
+                }
+
+            }
+            catch(Exception e)
+            {
+
+                Console.WriteLine("Error con la lectura del archivo");
+
+            }
+            finally
+            {
+
+                if (archivo != null) archivo.Close();
+
+                Console.WriteLine("conexión con el fichero cerrada");
+
+            }
 
         }
     }
